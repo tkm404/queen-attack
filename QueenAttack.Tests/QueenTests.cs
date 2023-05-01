25be572428837879f4c2ddd596ae45c9439a8042
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-01 14:06:59 -0700

    add X and Y fields to queen class
    Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>
    dquote> Co-Authored-By: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- QueenAttack.Tests/QueenTests.cs
+++ QueenAttack.Tests/QueenTests.cs
@@ -12,8 +12,18 @@ namespace QueenAttack.Tests
     public void
     QueenConstructor_CreatesInstanceOfQueen_Queen()
     {
-      Queen newQueen = new Queen();
+      Queen newQueen = new Queen(2, 4);
       Assert.AreEqual(typeof(Queen), newQueen.GetType());
+    }
+
+    [TestMethod]
+    public void QueenCoord_SetsCoordsForQueen_Queen()
+    {
+      int xPos = 2;
+      int yPos = 4;
+      Queen newQueen = new Queen (xPos, yPos);
+      Assert.AreEqual(xPos, newQueen.XCoord);
+      Assert.AreEqual(yPos, newQueen.YCoord);
     }
   }
 }
