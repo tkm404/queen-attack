853b3d128adb95b3e686962a3b60fe047ffba369
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-01 16:13:42 -0700

    add intro UI
    dquote> Co-Authored-By: Kyle Crawford <kcraw217@gmail.com>
    dquote> Co-Authored-By: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- QueenAttack/Program.cs
+++ QueenAttack/Program.cs
@@ -10,6 +10,29 @@ namespace QueenAttack
     static void Main()
     {
       Console.WriteLine($"{Banner.Welcome}");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to Queen Attack!!");
+      Console.WriteLine("Enter in a x-coord for the King piece (1-8)");
+      string stringKingX = Console.ReadLine();
+      Console.WriteLine("Enter in a y-coord for the King piece (1-8)");
+      string stringKingY = Console.ReadLine();
+      Console.WriteLine("Enter in a x-coord for the Queen piece (1-8)");
+      string stringQueenX = Console.ReadLine();
+      Console.WriteLine("Enter in a y-coord for the Queen piece (1-8)");
+      string stringQueenY = Console.ReadLine();
+      Console.WriteLine("Let's BATTLE!");
+      int kingXPos = int.Parse(stringKingX);
+      int kingYPos = int.Parse(stringKingY);
+      Piece king = new Piece(kingXPos, kingYPos, "King");
+      king.GetRemainderSum();
+      Console.WriteLine($"King remainder: {king.Remainder}");
+      int queenXPos = int.Parse(stringQueenX);
+      int queenYPos = int.Parse(stringQueenY);
+      Piece queen = new Piece(queenXPos, queenYPos, "Queen");
+      queen.GetRemainderSum();
+      Console.WriteLine($"Queen remainder: {queen.Remainder}");
     }
+
+
   }
 }
